6459525686c6f7ba22877e718ec378f06c02374e
Ayush-user <ayush.singh03042005@gmail.com>
2025-09-02 23:52:38 +0530

    Time: 1 ms (71.51%) | Memory: 46.1 MB (8.57%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,40 +1,40 @@
 class Solution {
-    static int value=Integer.MIN_VALUE+10;
     public int[][] setZeroes(int[][] matrix) {
+        // int col[m]=matrix[0][...]
+        // int row[n]=matrix[...][0]
+        int col0=1;
         for(int i=0;i<matrix.length;i++) {
             for(int j=0;j<matrix[i].length;j++) {
                 if(matrix[i][j]==0) {
-                    changeRow(i,matrix);
-                    changeCol(j,matrix);
+                    matrix[i][0]=0;
+                    if(j!=0) {
+                    matrix[0][j]=0;
+
+                    } else{
+                        col0=0;
+                    }
                 }
             }
         }
-        changeVal(matrix);
+
+            for(int i=1;i<matrix.length;i++) {
+                for(int j=1;j<matrix[i].length;j++) {
+                    if(matrix[0][j]==0 || matrix[i][0]==0) {
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+            if(matrix[0][0]==0) {
+                for(int j=0;j<matrix[0].length;j++) {
+                    matrix[0][j]=0;
+                }
+            }
+            if(col0==0) {
+                for(int i=0;i<matrix.length;i++) {
+                    matrix[i][0]=0;
+                }
+            }
+        
         return matrix;
-    }
-    public void changeRow(int i,int[][]matrix) {
-        for(int j=0;j<matrix[i].length;j++) {
-            if(matrix[i][j]!=0 ) {
-                matrix[i][j]=value;
-            }
-        }
-    }
-
-    public void changeCol(int j,int[][] matrix) {
-        for(int i=0;i<matrix.length;i++) {
-            if(matrix[i][j]!=0 ) {
-                matrix[i][j]=value;
-            }
-        }
-    }
-
-    public void changeVal(int[][] matrix) {
-        for(int i=0;i<matrix.length;i++) {
-            for(int j=0;j<matrix[i].length;j++) {
-                if(matrix[i][j]==value) {
-                    matrix[i][j]=0;
-                }
-            }
-        }
     }
 }
