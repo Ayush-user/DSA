055cd8050641ea45c5b649ff3d88eb2a20321e5c
Ayush-user <ayush.singh03042005@gmail.com>
2025-09-15 23:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (19.96%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    static int search(int[] nums, int target) {
         int pivot = findPivot(nums);
 
         if (pivot == -1) {
@@ -19,6 +19,7 @@ class Solution {
 
     static int binarySearch(int[] arr, int target, int start, int end) {
         while (start <= end) {
+
             int mid = start + (end - start) / 2;
 
             if (target < arr[mid]) {
@@ -52,40 +53,4 @@ class Solution {
         }
         return -1;
     }
-
-    static int findPivotWithDuplicates(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            }
-
-            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
-
-                if (start < end && arr[start] > arr[start + 1]) {
-                    return start;
-                }
-                start++;
-
-                if (end > start && arr[end] < arr[end - 1]) {
-                    return end - 1;
-                }
-                end--;
-            }
-
-            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
-
 }
