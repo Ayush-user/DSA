75608d8a5112121961517a8c4bb41dceb8608311
Ayush-user <ayush.singh03042005@gmail.com>
2026-09-15 17:57:59 +0530

    Time: 1 ms (76.93%) | Memory: 46.6 MB (62.77%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,23 +1,43 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0)
-            return 0;
 
-        int[] temp = new int[nums.length];
-        int j = 0;
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] != nums[i + 1]) {
-                temp[j++] = nums[i];
+        int i=0;
+        int j=i+1;
+        int k=j;
+        while(i<nums.length && j< nums.length) {
+            if(nums[i]==nums[j]) {
+                // k=j;
+                j++;
+            } else {
+                int temp=nums[k];
+                nums[k]=nums[j];
+                nums[j]=temp;
+                i++;
+                j++;
+                k++;
             }
         }
+        return k;
 
-        temp[j++] = nums[nums.length - 1];
 
-        for (int k = 0; k < j; k++) {
-            nums[k] = temp[k];
-        }
+        // if (nums.length == 0)
+        //     return 0;
 
-        return j;
+        // int[] temp = new int[nums.length];
+        // int j = 0;
+
+        // for (int i = 0; i < nums.length - 1; i++) {
+        //     if (nums[i] != nums[i + 1]) {
+        //         temp[j++] = nums[i];
+        //     }
+        // }
+
+        // temp[j++] = nums[nums.length - 1];
+
+        // for (int k = 0; k < j; k++) {
+        //     nums[k] = temp[k];
+        // }
+
+        // return j;
     }
 }
